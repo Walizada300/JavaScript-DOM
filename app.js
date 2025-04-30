105830c8bbf4869d295fb474eec1fbae97c60eed
baes_walizada <walizada.300@gmail.com>
2025-04-30 22:40:06 +0430

    First Version

---
+++ app.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#form")
+const title = document.querySelector("#title")
+const price = document.querySelector("#price")
+const products = document.querySelector("#products")
+
+const addProduct = (event) => {
+    event.preventDefault()
+    const productItem = document.createElement('li')
+    const productInfo = document.createTextNode(`عنوان: ${title.value} - قیمت: ${price.value}`)
+    productItem.appendChild(productInfo)
+    products.appendChild(productItem)
+    title.value = ''
+    price.value = ''
+}
+
+form.addEventListener('submit', addProduct)
